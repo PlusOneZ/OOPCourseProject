43df6ca759c3cad27eb1be704b047b5386325abd
LJSRabbit <2415946945@qq.com>
2020-06-03 19:21:20 +0800

    no message

---
+++ Resources/map/SafeMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="SafeMap" tilewidth="1280" tileheight="720" tilecount="1" columns="1">
+ <image source="SafeMap.png" width="1280" height="720"/>
+</tileset>
